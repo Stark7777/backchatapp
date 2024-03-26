6291937349301e1df7e473bbea4ec0651de8418a
Starkk777 <jimmyleiva07@gmail.com>
2024-03-26 10:47:00 -0600

    Correcion en archivo index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
+const app = express(); 
+const cors  = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const agentRoutes = require("./routes/agentRoutes");
@@ -9,34 +9,126 @@ const queueRoute = require("./routes/queueRoute");
 const socket = require("socket.io");
 const agentSessionModel = require("./models/agentSessionModel");
 
-// Modelos temporales, puedes eliminar o comentar estas líneas si ya no son necesarios
+//temp
 const queueModel = require("./models/queueModel");
 const messageModel = require("./models/messageModel");
 
 dotenv.config();
-
-// Configuración de CORS
+//app.use(cors());
 app.use(cors({
-  origin: "https://chatappclient-uqau.onrender.com", // URL de tu cliente React
-  credentials: true,
-}));
+    origin: "https://chatappclient-uqau.onrender.com", // URL de tu cliente React
+    credentials: true,
+  }));
 app.use(express.json());
 
-// Variables globales, ajusta según sea necesario
-global.onlineUser = new Map();
+//store all online users inside this map
+//global.onlineAgent =  new Map();
+global.onlineUser =  new Map();
 global.io = null;
 
-// Rutas de la API
 app.use("/api/auth", agentRoutes);
 app.use("/api/message", messageRoute);
-app.use("/api/chat", queueRoute);
+app.use("/api/chat", queueRoute)
 
-// Conexión a MongoDB
 mongoose.set('strictQuery', true);
+//mongoose connection
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => {
-    console.log("DB Connection Successful!");
-    // Limpieza inicial (esto es opcional, depende de tus requerimientos)
-    agentSessionModel.deleteMany({}).then(() => console.log
+    }).then(() => {
+        
+        console.log("DB Connection Successful!");
+
+        agentSessionModel.deleteMany({}).then(x => console.log('agentSessionModel ok'));
+        queueModel.deleteMany({}).then(x => console.log('queueModel ok'));
+        messageModel.deleteMany({}).then(x => console.log('messageModel ok'));
+
+    }).catch((err) => console.log(err));
+
+
+
+const server = app.listen(process.env.PORT, ()=>{
+    console.log(`Server started on Port ${process.env.PORT}`);
+});
+
+// status
+app.get('/status', (request, response) => {
+    const status = {
+       'Status': 'Running'
+    };
+    
+    response.status(200).send(status);
+});
+
+
+const io = socket(server,{
+    cors: {
+        origin: "https://chatappclient-uqau.onrender.com/",
+        credentials: true,
+    },
+});
+
+ 
+io.on("connection",(socket)=>{
+    
+    console.log(`socket.id: ${socket.id} - socket.sessionId: ${socket.sessionId} - socket.agent: ${socket.agent}`);
+    
+    // join the "sessionId" room
+    socket.join(socket.sessionId);
+
+});
+
+// io middleware
+io.use(async (socket, next) => {
+    
+    const userId = socket.handshake.auth.userId;
+    if (!userId) return next(new Error("userId requerid"));
+
+    const filter = { agent: userId }
+    const agentSession = await agentSessionModel.findOne(filter);
+
+    if (agentSession) {
+        console.log(`Exist session session.agent: ${agentSession.agent} - session.connect: ${agentSession.connected}  - session._id: ${agentSession._id}`);
+        socket.sessionId = agentSession._id.toString();
+        socket.agent = agentSession.agent;
+        socket.connected = agentSession.connected;
+        return next();
+    }
+
+    // create new session
+    const newSessionId = await createSession({
+        agent: userId,
+        connected: true,
+    });
+
+    if(! newSessionId) return next(new Error("error create session"));
+    socket.sessionId = newSessionId.toString();
+    socket.agent = userId;
+    socket.connected = true;
+
+    console.log(`New session session.agent: ${userId} - session.connect: ${true}  - session._id: ${newSessionId}`);
+
+    next();
+});
+
+global.io = io;
+
+
+
+async function createSession (data){
+    const item = await agentSessionModel.create({
+        connected: data.connected,
+        agent:data.agent
+    });
+
+    if(!item) return null;
+
+    return item._id
+}
+
+// Todo:
+/*
+queueContoller, add user to queue when no exist agent
+what happends with the socket's sessions if server crash
+chats.jsx emit new user also, get user when is the first connection
+*/
